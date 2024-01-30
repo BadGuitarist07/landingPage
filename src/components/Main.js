158e30c0dacd0bcd3e16dab70621b60850f21a1a
BadGuitarist07 <BadGuitarist07@gmail.com>
2024-01-30 16:11:18 -0500

    Test CI/CD v3

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ function Main () {
                 <div className='img-main'>
                 </div>
                 <div className='text-main'>
-                    <h1>Build The Community Your Fans Will Love</h1>
+                    <h1>Build The Community Your Fans Will Love It.</h1>
                     <p>Huddle re-imagines the way we build communities. You have
                         a voice, but so does your audience. Create connections with
                         your users as you engage in genuine discussion.
